76b2b07e48fb40d42ff8830956a6b6f140ce291a
Yang, Bo <atry@fb.com>
2022-04-28 22:16:11 +0000

    Add nix build files
    Test Plan:
    ```
    nix profile install --option experimental-features 'nix-command flakes' --option sandbox false 'git+https://github.com/Atry/hhvm.git?submodules=1&ref=nix&shallow=1'
    ```
    Or
    ```
    nix profile install --option experimental-features 'nix-command flakes' --option sandbox false "git+file://$(pwd)?submodules=1&shallow=1"
    ```

---
+++ flake.nix
@@ -0,0 +1,186 @@
+rec {
+  description = "High-performance JIT compiler for PHP/Hack";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: rec {
+    packages =
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+      ] (
+        system:
+          with import nixpkgs {
+            inherit system;
+            overlays = [
+              (self: {
+                lib,
+                stdenv,
+                fetchurl,
+                libelf,
+                zlib,
+                ...
+              }: {
+                # This overlay is a polyfill of the following PR and can be removed
+                # once the PR get merged:
+                #   https://github.com/NixOS/nixpkgs/pull/170927
+                libdwarf_0_4 = stdenv.mkDerivation rec {
+                  pname = "libdwarf";
+                  version = "0.4.0";
+
+                  src = fetchurl {
+                    url = "https://www.prevanders.net/libdwarf-${version}.tar.xz";
+                    # Upstream displays this hash broken into four parts:
+                    sha512 =
+                      "30e5c6c1fc95aa28a014007a45199160"
+                      + "e1d9ba870b196d6f98e6dd21a349e9bb"
+                      + "31bba1bd6817f8ef9a89303bed056218"
+                      + "2a7d46fcbb36aedded76c2f1e0052e1e";
+                  };
+
+                  configureFlags = ["--enable-shared" "--disable-nonshared"];
+
+                  buildInputs = [libelf zlib];
+
+                  meta = {
+                    homepage = "https://www.prevanders.net/dwarf.html";
+                    platforms = lib.platforms.unix;
+                    license = lib.licenses.lgpl21Plus;
+                  };
+                };
+              })
+            ];
+          }; let
+            stdenv =
+              if hostPlatform.isLinux
+              then gcc10Stdenv
+              else llvmPackages_13.stdenv;
+          in {
+            default = stdenv.mkDerivation rec {
+              pname = "hhvm";
+              version = builtins.substring 0 8 self.lastModifiedDate;
+
+              src = ./.;
+              nativeBuildInputs = [
+                cmake
+                pkg-config
+                flex
+                bison
+                cacert
+                python3
+                patchelf
+                which
+                unixtools.getconf
+              ];
+              buildInputs =
+                [
+                  boost
+                  libunwind
+                  libmemcached
+                  pcre
+                  gdb
+                  git
+                  perl
+                  libevent
+                  gd
+                  curl
+                  libxml2
+                  icu
+                  openssl
+                  zlib
+                  expat
+                  oniguruma
+                  libmcrypt
+                  tbb
+                  gperftools
+                  bzip2
+                  openldap
+                  editline
+                  libelf
+                  glog
+                  libpng
+                  libxslt
+                  libkrb5
+                  gmp
+                  libedit
+                  libvpx
+                  imagemagick6
+                  fribidi
+                  gperf
+                  re2c
+                  sqlite
+                  libgccjit
+                  re2
+                  tzdata
+                  lz4
+                  double-conversion
+                  brotli
+                  libzip
+                  zstd
+                  jemalloc
+                  fmt
+                  libsodium
+                  unzip
+                  freetype
+                  gettext
+                ]
+                ++ lib.optionals hostPlatform.isLinux [
+                  libcap
+                  uwimap
+                  numactl
+                  linux-pam
+                  libdwarf
+                ]
+                ++ lib.optionals hostPlatform.isMacOS [
+                  libdwarf_0_4
+                ];
+
+              cmakeFlags = [
+                "-DHAVE_SYSTEM_TZDATA:BOOL=ON"
+                "-DHAVE_SYSTEM_TZDATA_PREFIX=${tzdata}/share/zoneinfo"
+                "-DMYSQL_UNIX_SOCK_ADDR=/run/mysqld/mysqld.sock"
+              ];
+
+              prePatch = ''
+                patchShebangs .
+              '';
+
+              preBuild =
+                ''
+                  set -e
+                  make \
+                    -f third-party/rustc/CMakeFiles/bundled_rust.dir/build.make \
+                    third-party/rustc/bundled_rust-prefix/src/bundled_rust-stamp/bundled_rust-patch
+                  patchShebangs \
+                    third-party/rustc/bundled_rust-prefix/src/bundled_rust
+                ''
+                # Prebuilt rustc and cargo needs patch if HHVM is built on
+                # either NixOS or a Nix sandbox
+                + lib.optionalString hostPlatform.isLinux ''
+                  make \
+                    -f third-party/rustc/CMakeFiles/bundled_rust.dir/build.make \
+                    third-party/rustc/bundled_rust-prefix/src/bundled_rust-stamp/bundled_rust-install
+                  patchelf \
+                    --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
+                    --add-needed ${zlib}/lib/libz.so.1 \
+                    --add-rpath "${lib.makeLibraryPath [zlib stdenv.cc.cc.lib]}" \
+                    third-party/rustc/bundled_rust-prefix/bin/rustc
+                  patchelf \
+                    --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
+                    --add-needed ${zlib}/lib/libz.so.1 \
+                    --add-rpath "${lib.makeLibraryPath [zlib stdenv.cc.cc.lib]}" \
+                    third-party/rustc/bundled_rust-prefix/bin/cargo
+                '';
+
+              meta = {
+                inherit description;
+                platforms = builtins.attrNames packages;
+                homepage = "https://hhvm.com";
+                license = "PHP/Zend";
+              };
+            };
+          }
+      );
+  };
+}
