85abd8d28a5eefde0d4b62d3f43cd22e97998068
Yang, Bo <atry@fb.com>
2022-05-19 22:52:01 +0000

    Fix hphp/test/run.php path

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
           checks.quick = pkgs.runCommand "hhvm-quick-test" { } ''
             set -ex
             mkdir $out
-            HHVM_BIN="${packages.hhvm}/bin/hhvm" "${packages.hhvm}/bin/hhvm" ${./hphp/test/run.php} quick
+            HHVM_BIN="${packages.hhvm}/bin/hhvm" "${packages.hhvm}/bin/hhvm" ${./.}/hphp/test/run.php quick
           '';
 
           devShells.default = pkgs.mkShell ({
