21f50aee8fd5527d5b2d8c49d79c2df3c96d16a3
Yang, Bo <atry@fb.com>
2022-05-19 22:52:02 +0000

    Format

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
                   --version ${pkgs.lib.strings.escapeShellArg pkg.version} \
                   --description ${pkgs.lib.strings.escapeShellArg pkg.meta.description} \
                   --url ${pkgs.lib.strings.escapeShellArg pkg.meta.homepage} \
-                  --maintainer ${pkgs.lib.strings.escapeShellArg (pkgs.lib.strings.intersperse ", " (map ({name, email, ...}: "\"${name}\" <${email}>")pkg.meta.maintainers))} \
+                  --maintainer ${pkgs.lib.strings.escapeShellArg (pkgs.lib.strings.intersperse ", " (map ({name, email, ...}: "\"${name}\" <${email}>") pkg.meta.maintainers))} \
                   --license ${pkgs.lib.strings.escapeShellArg pkg.meta.license.spdxId} \
                   --after-install ${
                     pkgs.writeScript "after-install.sh" ''
